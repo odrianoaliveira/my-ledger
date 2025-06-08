c51a19efdc98642572d9e74e5a7b173d19937772
Adriano de Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2025-06-08 11:23:57 +0200

    chore(server): install ktor json serializer

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,10 @@ repositories {
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.kotlinx.json)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.content.negotiation)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
